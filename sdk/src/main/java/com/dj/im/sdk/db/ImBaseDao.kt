8239959761f5ede40c4a17212b02b970e7e39bb5
陈雷 <903736662@qq.com>
2021-03-24 16:33:37 +0800

    mod: 数据库插入冲突策略优化

--- sdk/src/main/java/com/dj/im/sdk/db/ImBaseDao.kt
+++ sdk/src/main/java/com/dj/im/sdk/db/ImBaseDao.kt
@@ -2,6 +2,7 @@ package com.dj.im.sdk.db
 
 import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Update
 
 
@@ -14,13 +15,13 @@ internal interface ImBaseDao<T> {
     /**
      * 插入单条数据
      */
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(item: T?)
 
     /**
      * 插入list数据
      */
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertList(items: List<T?>?)
 
     /**
